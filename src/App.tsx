6799ec9fd84d53859cef36f3d5d12987c1d74f49
Valeria <vzampetti@correo.unimet.edu.ve>
2023-03-11 18:29:09 -0400

    Login y Register Validados

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import AuthProvider from "./contexts/AuthProvider";
 import ProtectedLayout from "./layouts/ProtectedLayout";
 import UserLayout from "./layouts/UserLayout";
@@ -8,34 +9,36 @@ import Home from "./pages/movies/Home";
 import Searcher from "./pages/Searcher";
 import Login from "./pages/users/Login";
 import Register from "./pages/users/Register";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<UserLayout />}>
-            <Route index element={<Searcher />} />
-          </Route>
+    return (
+        <AuthProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<UserLayout />}>
+                        <Route index element={<Searcher />} />
+                    </Route>
 
-          <Route path="/users" element={<UserLayout />}>
-            <Route index element={<Navigate to="login" />} />
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="*" element={<Error404 />} />
-          </Route>
+                    <Route path="/users" element={<UserLayout />}>
+                        <Route index element={<Navigate to="login" />} />
+                        <Route path="login" element={<Login />} />
+                        <Route path="register" element={<Register />} />
+                        <Route path="*" element={<Error404 />} />
+                    </Route>
 
-          <Route path="/movies" element={<ProtectedLayout />}>
-            <Route index element={<Home />} />
-            <Route path="movie/:id" element={<Detail />} />
-            <Route path="*" element={<Error404 />} />
-          </Route>
+                    <Route path="/movies" element={<ProtectedLayout />}>
+                        <Route index element={<Home />} />
+                        <Route path="movie/:id" element={<Detail />} />
+                        <Route path="*" element={<Error404 />} />
+                    </Route>
 
-          <Route path="*" element={<Error404 />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
-  );
+                    <Route path="*" element={<Error404 />} />
+                </Routes>
+            </BrowserRouter>
+            <ToastContainer />
+        </AuthProvider>
+    );
 }
 
 export default App;
