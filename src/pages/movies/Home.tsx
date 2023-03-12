2d1bfdd51b569d6b76faa056078b0c3af17edfc0
Sergionx <sergio180203@gmail.com>
2023-03-11 22:24:09 -0400

    added moviecard

--- src/pages/movies/Home.tsx
+++ src/pages/movies/Home.tsx
@@ -1,18 +1,30 @@
-import React, { useEffect, useState } from 'react'
-import { Movie } from '../../interfaces/Movie'
+import React, { useEffect, useState } from "react";
+import MovieCard from "../../components/MovieCard";
+import { getMovies } from "../../firebase/api";
+import { Movie } from "../../interfaces/Movie";
 
 function Home() {
-  const [movies, setMovies] = useState<Movie[]>([])
-
+  const [movies, setMovies] = useState<Movie[]>([]);
 
   useEffect(() => {
-      
-      console.log('Home')
-  }, [])
+    async function initializeMovies() {
+      setMovies(await getMovies());
+    }
+
+    initializeMovies();
+    console.log("Home");
+  }, []);
 
   return (
-    <div>Home</div>
-  )
+    <div>
+      dsd
+      <div className="flex flex-wrap gap-3">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
