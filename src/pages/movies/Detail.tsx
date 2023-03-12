09c00f618ce74ca4f82a92537ab390507d394364
Valeria <vzampetti@correo.unimet.edu.ve>
2023-03-12 14:19:21 -0400

    Detail bonito

--- src/pages/movies/Detail.tsx
+++ src/pages/movies/Detail.tsx
@@ -7,6 +7,18 @@ function Detail() {
   let { id } = useParams();
   let [movie, setMovie] = useState<Movie | null>(null);
 
+  const imgStyle = {
+    backgroundImage: `url(${getMoviePoster(movie!.backdrop_path)})`,
+    backgroundSize: "cover",
+    backgroundPosition: "left calc((50vw - 170px) - 340px) top",
+    backgroundRepeat: "no-repeat",
+  } as React.CSSProperties;
+
+  const colorStyle = {
+    backgroundImage:
+      "linear-gradient(to right, rgba(31.5, 31.5, 31.5, 1) calc((50vw - 170px) - 340px), rgba(31.5, 31.5, 31.5, 0.84) 50%, rgba(31.5, 31.5, 31.5, 0.84) 100%)",
+  } as React.CSSProperties;
+
   useEffect(() => {
     const fetchData = async () => {
       if (!id) {
@@ -20,56 +32,74 @@ function Detail() {
   }, []);
   //let { title, vote_average, vote_count, original_language, genre_ids, adult} = fetchedData;
 
+  function toCurrency(number: number): string {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(number);
+  }
   if (!movie) {
     return <div> Loading... </div>;
   } else {
     return (
-      <div className="relative  flex flex-row gap-2">
-        <div className="basis-1/3">
-          <img
-            src={getMoviePoster(movie!.poster_path)}
-            className="h-[20rem] w-[15rem] object-cover rounded-xl "
-          />
-        </div>
-        <div className="card-body basis-2/3">
-          <h5 className="card-title text-4xl font-bold"> {movie?.title} </h5>
-          <h5>
-            {movie?.release_date} {movie?.original_language} {movie?.status}
-          </h5>
-          <p className="card-text">
-            {" "}
-            <h3 className="font-bold text-xl "> Overview </h3>
-            {movie?.overview}
-            <div className=" flex flex-row gap-6">
-              <div>
-                <h1 className="text-lg font-semibold"> Presupuesto</h1>
-                <h3 className="text-lg"> Budget {movie?.budget} </h3>
+      <>
+        <div style={imgStyle}>
+          <div style={colorStyle}>
+            <div className="flex flex-row gap-2 py-4 px-12 text-white">
+              <div className="basis-1/3">
+                <img
+                  src={getMoviePoster(movie!.poster_path)}
+                  className="h-[20rem] w-[15rem] object-cover rounded-xl "
+                />
               </div>
-              <div>
-                <h1 className="text-lg font-semibold">Companias</h1>
-                {movie?.production_companies?.map((company) => (
-                  //Create a chip for each company
-                  <div className="flex flex-row gap-2 bg-slate-400 rounded-xl p-3">
-                    <img
-                      src={getMoviePoster(company.logo_path)}
-                      className="h-[2rem] w-[2rem] object-cover rounded-xl"
-                    />
-                    <h3 className="text-lg"> {company.name} </h3>
+
+              <div className="card-body basis-2/3">
+                <h5 className="card-title text-4xl font-bold">
+                  {movie?.title}{" "}
+                </h5>
+                <h5>
+                  {movie?.release_date} {movie?.original_language}{" "}
+                </h5>
+                <section className="flex flex-row gap-2 my-3">
+                  {movie?.genres?.map((genre) => (
+                    <h5 className="text-lg font-semibold bg-terciary3 rounded-lg px-2">
+                      {" "}
+                      {genre.name}{" "}
+                    </h5>
+                  ))}
+                </section>
+                <p className="card-text">
+                  {" "}
+                  <h3 className="font-bold text-xl "> Overview </h3>
+                  {movie?.overview}
+                  <div className=" flex flex-row justify-between">
+                    <div>
+                      <h1 className="text-lg font-semibold"> Presupuesto</h1>
+                      <h3 className="text-lg">
+                        {" "}
+                        Budget {toCurrency(movie?.budget)}{" "}
+                      </h3>
+                    </div>
+
+                    <div>
+                      <h1 className="text-lg font-semibold">Companias</h1>
+                      <div className="flex flex-col">
+                        {movie?.production_companies?.map((company) => (
+                          //Create a chip for each company
+                          <img
+                            src={getMoviePoster(company.logo_path)}
+                            className="h-auto w-[7rem] object-cover rounded-xl"
+                          />
+                        ))}
+                      </div>
+                    </div>
                   </div>
-                ))}
-              </div>
-              <div>
-                <h3 className="text-lg font-semibold"> Géneros </h3>
-                {movie?.genres?.map((name) => (
-                  <div className="flex flex-row gap-2 ">
-                    <h3 className="text-lg"> {name.name} </h3>
-                  </div>
-                ))}
+                </p>
               </div>
             </div>
-          </p>
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 }
