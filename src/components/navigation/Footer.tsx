29bd1afd22e556d22d6809f01bb009112623e9d8
Valeria <vzampetti@correo.unimet.edu.ve>
2023-03-12 19:18:15 -0400

    Cambio Footer

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -2,52 +2,30 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export const Footer = () => (
-    <footer className="bg-primary/50 flex flex-row text-lg justify-between border-t-[1px] p-4 border-[black] w-full">
-        <ul className="flex flex-col gap-4">
-            <li>
-                <h2 className="font-bold">Atencion al cliente</h2>
-                <p>ayuda@gmail.com</p>
-            </li>
+  <footer className="bg-primary/50 flex flex-row text-lg justify-between border-t-[1px] p-4 border-[black] w-full">
+    <ul className="flex flex-col gap-4">
+      <li>
+        <h2 className="font-bold">Atencion al cliente</h2>
+        <p>ayuda@gmail.com</p>
+      </li>
 
-            <Link
-                to="/users/Login"
-                className="text-black hover:text-blue-700 cursor-pointer"
-            >
-                Centro de ayuda
-            </Link>
-        </ul>
+      <li>
+        <h2 className="font-bold">Servicio</h2>
+        <p>servicio@gmail.com</p>
+      </li>
+    </ul>
 
-        <ul className="flex flex-col gap-4">
-            <Link
-                to="/users/Login"
-                className="text-black hover:text-blue-700 cursor-pointer"
-            >
-                Términos de uso
-            </Link>
-
-            <Link
-                to="/users/Login"
-                className="text-black hover:text-blue-700 cursor-pointer"
-            >
-                Privacidad
-            </Link>
-        </ul>
-
-        <ul className="flex flex-col">
-            <Link
-                to="/users/Login"
-                className="text-black hover:text-blue-700 cursor-pointer"
-            >
-                Sobre nosotros
-            </Link>
-            <Link
-                to="/users/Login"
-                className="text-black hover:text-blue-700 cursor-pointer"
-            >
-                Preguntas frecuentes
-            </Link>
-        </ul>
-    </footer>
+    <ul className="flex flex-col">
+      <li>
+        <h2 className="font-bold">Contactanos!</h2>
+        <p>0424-128282882822</p>
+      </li>
+      <li>
+        <h2 className="font-bold">Usuario de ig</h2>
+        <p>ServicioPelis</p>
+      </li>
+    </ul>
+  </footer>
 );
 
 export default Footer;
