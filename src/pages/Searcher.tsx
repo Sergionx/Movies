180e73896225cb058c490e8a63a43f9949aa9f82
Sergionx <sergio180203@gmail.com>
2023-03-12 20:33:32 -0400

    display upcmoming at the start

--- src/pages/Searcher.tsx
+++ src/pages/Searcher.tsx
@@ -3,7 +3,7 @@ import { useBottomScrollListener } from "react-bottom-scroll-listener";
 import Loading from "../components/Loading";
 import MovieCard from "../components/MovieCard";
 import SearchBar from "../components/SearchBar";
-import { getMovies, getSearchMovie } from "../firebase/api";
+import { getMovies, getMovieUpcoming, getSearchMovie } from "../firebase/api";
 import { Movie } from "../interfaces/Movie";
 
 function Searcher() {
@@ -11,6 +11,15 @@ function Searcher() {
   const [inputValue, setInputValue] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const [loading, setLoading] = useState(false);
+  const [searched, setSearched] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    getMovieUpcoming(1).then((movies) => {
+      setMovies(movies);
+      setLoading(false);
+    });
+  }, []);
 
   useBottomScrollListener(addMovies);
 
@@ -22,11 +31,10 @@ function Searcher() {
     if (newMovies.length === 0) {
       setLoading(false);
       return;
-    };
+    }
     setMovies([...movies, ...newMovies]);
     setCurrentPage(currentPage + 1);
     setLoading(false);
-
   }
 
   function handleSearch(value: string) {
@@ -34,12 +42,47 @@ function Searcher() {
     setCurrentPage(1);
 
     if (value.length > 2) {
+      setSearched(true);
       setLoading(true);
       getSearchMovie(value, currentPage).then((movies) => {
         setMovies(movies);
         setLoading(false);
       });
     }
+  }
+
+  if (!searched) {
+    return (
+      <div className="flex flex-col justify-center self-center bg-terciary1 p-4">
+        <div className="w-[40%] self-center">
+          <SearchBar handleSearch={handleSearch} searchValue={inputValue} />
+        </div>
+
+        <div
+          className={`mt-4 ${loading && movies.length !== 0 && "min-h-[60rem]"}
+      border-[5px] rounded-xl border-secondary p-2 bg-terciary3 `}
+        >
+          <h1 className="text-3xl font-bold text-center text-primary">
+            Próximos estrenos
+          </h1>
+
+          {loading && (
+            <Loading
+              svgClass="h-[20rem] w-[20rem]"
+              statusClass={`absolute -translate-x-1/2 -translate-y-1/2 ${
+                movies.length >= 15 ? "top-2/4" : "top-[90%]"
+              } left-1/2 z-50`}
+            />
+          )}
+
+          <div className="flex flex-wrap gap-5 mt-4 justify-center">
+            {movies.map((movie) => (
+              <MovieCard key={movie.id} movie={movie} />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
   }
 
   return (
@@ -49,7 +92,7 @@ function Searcher() {
       </div>
 
       <div
-        className={`mt-4 ${(loading && movies.length !== 0) && "min-h-[60rem]"}
+        className={`mt-4 ${loading && movies.length !== 0 && "min-h-[60rem]"}
         border-[5px] rounded-xl border-secondary p-2 bg-terciary3 `}
       >
         {movies.length === 0 ? (
